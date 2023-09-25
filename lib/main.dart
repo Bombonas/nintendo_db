d1a6d493d1bfd59ff69dddc25dff3d95e19b7a10
Bombonas <pablomejiast@gmail.com>
2023-09-25 18:24:12 +0200

    commitwe

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,30 @@
+import 'dart:io' show Platform;
 import 'package:flutter/material.dart';
+import 'package:window_manager/window_manager.dart';
+import 'app.dart';
 
-void main() {
-  runApp(const MyApp());
+void main() async {
+// En aplicacions d'escriptori, cridar la funció 'showWindow'
+  try {
+    if (Platform.isLinux || Platform.isMacOS || Platform.isWindows) {
+    WidgetsFlutterBinding.ensureInitialized();
+    await WindowManager.instance.ensureInitialized();
+    windowManager.waitUntilReadyToShow().then(showWindow);
+    }
+  } catch (e) {
+    // ignore: avoid_print
+    print(e);
+  }
+
+  // Definir 'App' com el widget principal
+  runApp(const App());
+}
+
+// Al mostrar la finestra d'aplicació,
+// definir una mida de finestra mínima i el títol de la finestra
+void showWindow (_) async {
+  windowManager.setMinimumSize(const Size(300.0, 600.0));
+  await windowManager.setTitle('App');
 }
 
 class MyApp extends StatelessWidget {
